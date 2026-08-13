9e5d2a2c825d24444e6969f4947deae3476b637d
lcc <lidaryl@163.com>
2026-08-13 22:37:06 +0800

    add test

--- sample/composeApp/src/commonMain/kotlin/sample/app/ui/page/DetailScreen.kt
+++ sample/composeApp/src/commonMain/kotlin/sample/app/ui/page/DetailScreen.kt
@@ -1,6 +1,8 @@
 package sample.app.ui.page
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -8,6 +10,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.aleyn.navigation.annotations.Screen
+import com.aleyn.navigation.core.navigator.LocalNavBackStackState
+import com.aleyn.navigation.core.route.NavCenter
 
 /**
  * @author : Aleyn
@@ -22,10 +26,17 @@ fun DetailScreen(
     id: Int,
     detailViewModel: DetailViewModel = viewModel()
 ) {
-    Box(Modifier.fillMaxSize()) {
-        Text(
-            text = "DetailScreen filter=$filter id=$id",
-            modifier = Modifier.align(Alignment.Center)
-        )
+    val navController = LocalNavBackStackState.current
+    Box(Modifier.fillMaxSize().clickable{
+        NavCenter.navigate("https://www.app.cn/users/133/1")
+    }) {
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "DetailScreen filter=$filter id=$id")
+            Text(text = "BackStack depth = ${navController.screens.size}")
+            Text(text = "entryId (last) = ${(navController.current as? DetailScreenDestination)?.entryId?.takeLast(6)}")
+        }
     }
 }
